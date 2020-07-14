d6a0d66cdc5f1abc420562937a9cfe551bfb3237
Jake Bown <jakebown@gmail.com>
2020-07-14 07:25:05 +0100

    Added build folder for GH Pages

---
+++ build/precache-manifest.9af966454bc0191a1c50368fa60d0895.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "018ed092c74c60fbd2438cdec795d68f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f25f75acff18750fad0c",
+    "url": "/static/css/main.908e3553.chunk.css"
+  },
+  {
+    "revision": "4661d86bbccd316e51f5",
+    "url": "/static/js/2.31666226.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.31666226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f25f75acff18750fad0c",
+    "url": "/static/js/main.957cc15b.chunk.js"
+  },
+  {
+    "revision": "731175524d8ccd2dc713",
+    "url": "/static/js/runtime-main.1188f160.js"
+  }
+]);
